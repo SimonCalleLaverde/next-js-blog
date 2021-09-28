3928836af1492ee3d0aaa391682376f10ef1c736
Simon Calle Laverde <kyverde88@gmail.com>
2021-09-28 14:40:50 -0500

    Dynamic Routes (Next.js).
    - Creating pages for each blog-post using "dynamic routes".
    - Generated dynamic routes.
    
    -- Docs:
    https://nextjs.org/learn/basics/dynamic-routes/implement-getstaticprops

---
+++ pages/posts/[id].js
@@ -0,0 +1,42 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+// "getStaticProps" which fetches necessary data for the post with "id"
+// The post page is now using the getPostData function in getStaticProps to get the post data and return it as props
+export async function getStaticProps({ params }) {
+  // Fetches necessary data for the blog post using params.id
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+};
+
+// "getStaticPaths" which returns an array of possible values for "id"
+// Importing & using the "getAllPostIds" function inside getStaticPaths
+export async function getStaticPaths() {
+  // Returns a list of 'possible values' for id
+  const paths = getAllPostIds()
+
+  return {
+    paths,
+    fallback: false
+  }
+};
+
+// A React component to render the page
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      { postData.title }
+      <br />
+      { postData.id }
+      <br />
+      { postData.date }
+    </Layout>
+  )
+};
+// export default function Post() {
+//   return <Layout>...</Layout>
+// };
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,27 +1,29 @@
-// Imports
-import Link from "next/link";
-import Head from "next/head";
-import Layout from "../../components/layout";
+// // NO LONGER IN USE
 
-// First Post (Temporary)
-export default function FirstPost() {
-  return (
-    <Layout>
-      <Head>
-        <title>First Post Yei</title>
-      </Head>
+// // Imports
+// import Link from "next/link";
+// import Head from "next/head";
+// import Layout from "../../components/layout";
 
-      <h1>
-        First Post
-      </h1>
+// // First Post (Temporary)
+// export default function FirstPost() {
+//   return (
+//     <Layout>
+//       <Head>
+//         <title>First Post Yei</title>
+//       </Head>
 
-      {/*<Link href="/">
-        <a>
-          <h2>
-            Back to home
-          </h2>
-        </a>
-      </Link>*/}
-    </Layout>
-  )
-};
+//       <h1>
+//         First Post
+//       </h1>
+
+//       {/*<Link href="/">
+//         <a>
+//           <h2>
+//             Back to home
+//           </h2>
+//         </a>
+//       </Link>*/}
+//     </Layout>
+//   )
+// };
