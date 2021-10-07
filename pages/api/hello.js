8626e90bf4073e5be43a895a40a1736ba0cbf935
Simon Calle Laverde <kyverde88@gmail.com>
2021-10-06 19:23:05 -0500

    Deploying A "Next.js" App (To "Vercel" Example).
    - Push to GitHub (Already long ago).
    - Deploy to Vercel:
    º Creating a Vercel Account.
    º (Aside: Eliminating "pages/posts/first-post.js" which caused 'Build error'. Was completely commented, but/so causes error: "Found page without a React Component as default export").
    
    -- Vercel / Netlify (Very Similar). Digital Ocean (Other Choice).
    
    -- Temporary (Trying) "Vercel".

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,7 @@
+// API Routes (Example)
 export default function handler(req, res) {
   res.status(200).json({ text: 'Hello' })
 };
+
+// Creating API Routes: More info (https://nextjs.org/learn/basics/api-routes/creating-api-routes)
+// API Routes Details: More info (https://nextjs.org/learn/basics/api-routes/api-routes-details)
